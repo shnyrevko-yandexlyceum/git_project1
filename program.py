dfacf65cc1c26474959b2ab5382c11d1706b89bd
Kirill Shnyrev <k.shnyrev@mail.ru>
2021-12-08 17:07:09 +0300

    Second lesson

--- program.py
+++ program.py
@@ -1,7 +1,10 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+def my_super_function():
+    print('What an awesome print!')
 
 
 if __name__ == '__main__':
-    main()
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_super_function()
